b6bab6e47432b996c16e54f96d7588844f5b182b
Sam Liebman <sammyliebman@gmail.com>
2018-07-17 11:41:51 -0400

    Done.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # imported Flask and jsonify from flask
 # imported SQLAlchemy from flask_sqlalchemy
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 # initialized new flask app
@@ -34,12 +34,50 @@ class Tweet(db.Model):
 
 
 # DEFINE ROUTES THAT RETURN APPROPRIATE HTML TEMPLATES HERE
+@app.route('/users')
+def users():
+    users_dict = [user.to_dict() for user in User.query.all()]
+    return render_template('users.html',users = users_dict)
 
+@app.route('/users/<int:id>')
+def user_id(id):
+    users_dict = [user.to_dict() for user in User.query.all() if user.id == id][0]
+    return render_template('user_show.html', user=users_dict)
 
+@app.route('/users/<username>')
+def username(username):
+    users_dict = [user.to_dict() for user in User.query.all() if user.username.lower()==username.lower()][0]
+    return render_template('user_show.html', user=users_dict)
 
+@app.route('/tweets')
+def tweets():
+    tweets_dict = [tweet.to_dict() for tweet in Tweet.query.all()]
+    return render_template('tweets.html',tweets = tweets_dict)
 
+@app.route('/tweets/<int:id>')
+def tweet_id(id):
+    tweets_dict = [tweet.to_dict() for tweet in Tweet.query.all() if tweet.id ==id][0]
+    return render_template('tweet_show.html',tweet = tweets_dict)
 
+#Tweets that belong to a user by user_id
+@app.route('/users/<int:id>/tweets')
+def tweets_user_id(id):
+    users_dict = [user.to_dict() for user in User.query.all() if user.id==id][0]
+    return render_template('tweets.html', tweets=users_dict['tweets'])
 
+#Tweets that belong to a user by a user's name
+@app.route('/users/<username>/tweets')
+def tweets_username(username):
+    users_dict = [user.to_dict() for user in User.query.all() if user.username.lower()==username.lower()][0]
+    return render_template('tweets.html', tweets=users_dict['tweets'])
+
+#A single User that is associated to a tweet by its id
+@app.route('/tweets/<int:id>/user')
+def user_tweet_id(id):
+    tweet_with_id = [tweet.to_dict() for tweet in Tweet.query.all() if tweet.id ==id][0]
+    user_id = tweet_with_id['user_id']
+    user_with_tweet = [user.to_dict() for user in User.query.all() if user.id == user_id][0]
+    return render_template('user_show.html', user=user_with_tweet)
 
 # run flask application
 if __name__ == "__main__":
